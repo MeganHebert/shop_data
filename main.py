20ee0291e3f0b7efc73b61fa4f13dd3b6f773204
Tony <tonyera789@gmail.com>
2024-08-01 14:55:49 -0500

    Update main.py
    Added the generation for the following field names: product category, payment type, quantity, price, failure reason.

--- main.py
+++ main.py
@@ -1,7 +1,46 @@
-
+from faker import Faker
+from faker.providers import DynamicProvider
 
 def main():
     print("Running application...")
+    fake = Faker()
+    # Add the product category provider
+    fake.add_provider(getProductCategoryProvider())
+    fake.add_provider(getPaymentProvider())
+    fake.add_provider(getFailureProvider())
+
+
+    product_category = fake.category()
+    payment_type = fake.payment_type()
+    quantity = fake.pyint(min_value=1, max_value=20)
+    price = round(fake.pyfloat(min_value=1.00, max_value=200.00), 2)
+    failure_reason = fake.failure_reason()
+
+    print('Category: {0}, Payment Type: {1}, Quantity: {2}, Price: {3}, Failure Reason: {4}'.format(product_category, payment_type, quantity, price, failure_reason))
+
+def getProductCategoryProvider():
+    product_category_provider = DynamicProvider(
+        provider_name = 'category',
+        elements = ['Beauty', 'Books', 'Electronics', 'Fine Art', 'Health & Personal Care', 
+                        'Home & Garden', 'Music & DVD', 'Musical Instruments', 'Office Products', 
+                        'Outdoors', 'Pet Supplies', 'Software', 'Sports', 'Toys & Games', 'Video Games', 
+                        'Video, & Blu-ray', 'Watches'],
+    )
+    return product_category_provider
+
+def getPaymentProvider():
+    payment_provider = DynamicProvider (
+        provider_name = 'payment_type',
+        elements= ['Debit', 'Cash', 'Credit'],
+    )
+    return payment_provider
+
+def getFailureProvider():
+    failure_provider = DynamicProvider(
+        provider_name= 'failure_reason',
+        elements= ['payment failed', 'insufficient funds', 'network timeout', 'card declined'],
+    )
+    return failure_provider
 
 # Run main function
 if __name__ == "__main__":
