f1f03fc7d0dce9cd1c2fa860e437111ea0951853
Tony <tonyera789@gmail.com>
2024-08-01 15:12:17 -0500

    Update main.py
    Moved functions above the main function and updated the print statement along with a typo on the category list.

--- main.py
+++ main.py
@@ -1,30 +1,13 @@
 from faker import Faker
 from faker.providers import DynamicProvider
 
-def main():
-    print("Running application...")
-    fake = Faker()
-    # Add the product category provider
-    fake.add_provider(getProductCategoryProvider())
-    fake.add_provider(getPaymentProvider())
-    fake.add_provider(getFailureProvider())
-
-
-    product_category = fake.category()
-    payment_type = fake.payment_type()
-    quantity = fake.pyint(min_value=1, max_value=20)
-    price = round(fake.pyfloat(min_value=1.00, max_value=200.00), 2)
-    failure_reason = fake.failure_reason()
-
-    print('Category: {0}, Payment Type: {1}, Quantity: {2}, Price: {3}, Failure Reason: {4}'.format(product_category, payment_type, quantity, price, failure_reason))
-
 def getProductCategoryProvider():
     product_category_provider = DynamicProvider(
         provider_name = 'category',
         elements = ['Beauty', 'Books', 'Electronics', 'Fine Art', 'Health & Personal Care', 
                         'Home & Garden', 'Music & DVD', 'Musical Instruments', 'Office Products', 
                         'Outdoors', 'Pet Supplies', 'Software', 'Sports', 'Toys & Games', 'Video Games', 
-                        'Video, & Blu-ray', 'Watches'],
+                        'Video & Blu-ray', 'Watches'],
     )
     return product_category_provider
 
@@ -41,6 +24,23 @@ def getFailureProvider():
         elements= ['payment failed', 'insufficient funds', 'network timeout', 'card declined'],
     )
     return failure_provider
+
+def main():
+    print("Running application...")
+    fake = Faker()
+    # Add the product category provider
+    fake.add_provider(getProductCategoryProvider())
+    fake.add_provider(getPaymentProvider())
+    fake.add_provider(getFailureProvider())
+
+
+    product_category = fake.category()
+    payment_type = fake.payment_type()
+    quantity = fake.pyint(min_value=1, max_value=20)
+    price = round(fake.pyfloat(min_value=1.00, max_value=200.00), 2)
+    failure_reason = fake.failure_reason()
+
+    print(f'Category: {product_category}, Payment Type: {payment_type}, Quantity: {quantity}, Price: {price}, Failure Reason: {failure_reason}')
 
 # Run main function
 if __name__ == "__main__":
