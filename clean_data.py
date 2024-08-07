73b9b3fe823bcaf933e10c3606a574b1d320cd80
MeganHebert <cis.megan.hebert@gmail.com>
2024-08-07 14:55:13 -0500

    megan clean data

---
+++ clean_data.py
@@ -0,0 +1,26 @@
+from pyspark.context import SparkContext
+from pyspark.sql.session import SparkSession
+from pyspark.sql.functions import col
+
+
+sc = SparkContext.getOrCreate()
+sc.setLogLevel("WARN")
+ 
+spark = SparkSession(sc)
+
+path = sc.textFile("file:///home/username/data_team_2.csv")
+
+df = spark.read.csv(path)
+
+name_error_msg = ["46284y924", "BOOM!", "corrupted data", "ERROR!", "This is a bad row", "THisISN't a GoOd RoW"]
+prod_name_error_msg = ["46284y924", "BOOM!", "ERROR!", "This is a bad row", "Not This ONe"]
+
+filter_customerID = df.filter(~col('_c1').rlike('^[^0-9]*$') & (col('_c1') != '') & (col('_c1') != "46284y924"))
+
+filter_customer_name = filter_customerID.filter(~col('_c2').isin(name_error_msg))
+
+filter_productID = filter_customer_name.filter(~col('_c3').rlike('^[^0-9]*$') & (col('_c3') != '') & (col('_c3') != "46284y924"))
+
+filter_prod_name = filter_productID.filter(~col('_c4').isin(prod_name_error_msg))
+
+filter_prod_name.show()
