f364e9f53276c0714cd4bad5e74b29814285c323
M. Sean Davis Jr. <35494197+Rainmaker0102@users.noreply.github.com>
2024-08-08 14:43:23 -0400

    refactor for function

--- clean_data.py
+++ clean_data.py
@@ -2,6 +2,19 @@ from pyspark.context import SparkContext
 from pyspark.sql.session import SparkSession
 from pyspark.sql.functions import col
 
+def filtered_data_megan(df):
+    name_error_msg = ["46284y924", "BOOM!", "corrupted data", "ERROR!", "This is a bad row", "THisISN't a GoOd RoW"]
+    prod_name_error_msg = ["46284y924", "BOOM!", "ERROR!", "This is a bad row", "Not This ONe"]
+
+    filter_customerID = df.filter(~col('_c1').rlike('^[^0-9]*$') & (col('_c1') != '') & (col('_c1') != "46284y924"))
+
+    filter_customer_name = filter_customerID.filter(~col('_c2').isin(name_error_msg))
+
+    filter_productID = filter_customer_name.filter(~col('_c3').rlike('^[^0-9]*$') & (col('_c3') != '') & (col('_c3') != "46284y924"))
+
+    filter_prod_name = filter_productID.filter(~col('_c4').isin(prod_name_error_msg))
+    return filter_prod_name
+
 
 sc = SparkContext.getOrCreate()
 sc.setLogLevel("WARN")
@@ -12,15 +25,6 @@ path = sc.textFile("file:///home/username/data_team_2.csv")
 
 df = spark.read.csv(path)
 
-name_error_msg = ["46284y924", "BOOM!", "corrupted data", "ERROR!", "This is a bad row", "THisISN't a GoOd RoW"]
-prod_name_error_msg = ["46284y924", "BOOM!", "ERROR!", "This is a bad row", "Not This ONe"]
+df = filtered_data_megan(df)
 
-filter_customerID = df.filter(~col('_c1').rlike('^[^0-9]*$') & (col('_c1') != '') & (col('_c1') != "46284y924"))
-
-filter_customer_name = filter_customerID.filter(~col('_c2').isin(name_error_msg))
-
-filter_productID = filter_customer_name.filter(~col('_c3').rlike('^[^0-9]*$') & (col('_c3') != '') & (col('_c3') != "46284y924"))
-
-filter_prod_name = filter_productID.filter(~col('_c4').isin(prod_name_error_msg))
-
-filter_prod_name.show()
+df.show()
